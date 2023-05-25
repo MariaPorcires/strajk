7d7e1783bd9d536ff798c72905b30730456ae619
hellnomary <maria.porcires@outlook.com>
2023-05-25 13:04:23 +0200

    input test

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Som användare vill jag kunna boka datum och tid samt ange antal spelare så att
 Som användare vill jag kunna välja skostorlek för varje spelare så varje spelare får skor som passar.
 
     - Man ska kunna skriva in skostorlek i input
+    - Man ska kunna lägga till fler spelare
 
 Som användare vill jag kunna ta bort ett fält för skostorlek om jag råkade klicka i ett för mycket så jag inte boka skor i onödan.
     - Man ska kunna trycka på en knapp för att ta bort ett fält
--- strajk/cypress/e2e/strajk.cy.js
+++ strajk/cypress/e2e/strajk.cy.js
@@ -7,16 +7,25 @@ describe('Booking spec', () => {
       cy.get('.booking-info__details').should('be.visible')
     })
 
-    it('should display amount', () => {
-      const amount = 2;
+    it('should be able to type in input', () => {
+      const date = '2023-05-30';
+      const time = '18:00'
+      const bowlers = 2;
+      const lanes = 1;
       cy.wait(1000)
 
-      cy.get('.booking-info__who').type(amount)
+      cy.get('.booking-info__date').type(date)
+      cy.get('.booking-info__date').should('have.value', '2023-05-30')
+      cy.get('[name="time"]').type(time)
+      cy.get('[name="time"]').should('have.value', '18:00')
+      cy.get('.booking-info__who').type(bowlers)
       cy.get('.booking-info__who').should('have.value', 2)
+      cy.get('.booking-info__lanes').type(lanes)
+      cy.get('.booking-info__lanes').should('have.value', 1)
       
     })
   
     
   })
 
-  //have value
+  
