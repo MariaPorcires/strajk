ca852576940e778962d22f29f3abb44a89147d57
hellnomary <maria.porcires@outlook.com>
2023-06-01 10:39:21 +0200

    navigation test

--- strajk/cypress/e2e/navigation.cy.js
+++ strajk/cypress/e2e/navigation.cy.js
@@ -14,6 +14,43 @@ describe('Navigation spec', () => {
         cy.get('.top__title').should('have.text', 'See you soon!')
     })
 
+    it('should be able to go to confirmationpage with a booking and show See you soon!', () => {
+        const date = '2023-05-30';
+        const time = '18:00'
+        const bowlers = 2;
+        const lanes = 1;
+        cy.wait(1000)
+  
+        cy.get('.booking-info__date').type(date)
+        cy.get('.booking-info__date').should('have.value', '2023-05-30')
+        cy.get('[name="time"]').type(time)
+        cy.get('[name="time"]').should('have.value', '18:00')
+        cy.get('.booking-info__who').type(bowlers)
+        cy.get('.booking-info__who').should('have.value', 2)
+        cy.get('.booking-info__lanes').type(lanes)
+        cy.get('.booking-info__lanes').should('have.value', 1)
+        
+        const size = 38;
+        const secondSize = 41;
+        cy.wait(1000)
+  
+        cy.get('.shoes__button').click()
+        cy.get('.shoes__input').type(size)
+        cy.get('.shoes__input').should('have.value', 38)
+  
+        cy.get('.shoes__button').last().click()
+        cy.get('.shoes__input').last().type(secondSize)
+        cy.get('.shoes__input').last().should('have.value', 41)
+  
+        cy.get('.booking__button').click()
+        cy.wait(1000)
+        cy.get('.confirmation__price > p').last().should('contain', '340')
+
+        cy.get('.confirmation__button').click()
+        cy.get('.top__title').should('have.text', 'See you soon!')
+
+    })
+
     it('should be able to go to bookingpage from navigationpage', () => {
         cy.get('.navigation__icon').click()
         cy.get('.navigation__link').last().click()
